e442134c1120ed79c4f96b3a36b7c76652213038
Joseph Little <jlittle@bloominsurance.com>
2024-06-26 18:24:05 -0400

    Hybrid Maui/Blazor Boiler plate

---
+++ MauiApp/MauiAppDemo/MauiProgram.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace MauiAppDemo
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+            builder.Services.AddAuthorizationCore();
+            //Add Authentication
+            builder.Services.AddScoped<CustomAuthenticationStateProvider>();
+            //When asking for the default Microsoft one, give ours!
+            builder.Services.AddScoped<AuthenticationStateProvider>(s => s.GetRequiredService<CustomAuthenticationStateProvider>());
+
+
+#if DEBUG
+            builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MauiApp/MauiAppDemo/Models/LoginViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppDemo.Models
+{
+    internal class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public bool LoginFailureHidden { get; set; } = true;
+
+        public bool ValidateLogin(out string jwtToken)
+        {
+            if (Username.Equals("Test") && Password.Equals("Test"))
+            {
+                jwtToken = "123456";
+                return true;
+            }
+
+            //Not valid
+            jwtToken = null;
+            LoginFailureHidden = false;
+            return false;
+        }
+    }
+}
